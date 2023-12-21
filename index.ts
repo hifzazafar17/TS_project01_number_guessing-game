c5fde08aa40daa3b6eaa0f9bf0103476d2ebe15a
Hifza-Code-Journey <152292315+Hifza-Code-Journey@users.noreply.github.com>
2023-12-21 22:55:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+
+// Generate a random number between 1 and 10
+const systemGeneratedNo = Math.floor(Math.random() * 10) + 1;
+
+// Prompt the user to enter their guess
+const { userGuess }: { userGuess: number } = await inquirer.prompt({
+    type: "number",
+    name: "userGuess",
+    message: "Guess the number (between 1 and 10):"
+});
+
+// Check if the user's guess matches the system-generated number
+if (userGuess === systemGeneratedNo) {
+    console.log("Congratulations! You guessed it right.");
+} else {
+    console.log(`Oops! The correct number was ${systemGeneratedNo}. Better luck next time!`);
+
+}
+
+
